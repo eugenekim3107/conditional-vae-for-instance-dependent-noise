0220eacb6701c86db0ef84f06e729ab64dd2496b
eugenekim <eugenekim3107@gmail.com>
2023-03-14 03:02:11 -0700

    edited files

--- noise_generator_demo.py
+++ noise_generator_demo.py
@@ -1,32 +1,53 @@
 import argparse
 import os
 import torch
-import numpy
+import numpy as np
 import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--filename-X',
-    default='noisyData/X_10.pt',
-    help='Location of features X')
+    '--filename-X-clean',
+    default='MNIST/X.pt'
+)
 parser.add_argument(
-    '--filename-y',
+    '--filename-y-clean',
+    default='MNIST/y.pt'
+)
+parser.add_argument(
+    '--filename-y-noisy',
     default='noisyData/y_10.pt',
     help='Location of label y')
 parser.add_argument(
     '--number',
-    default=np.random.randint(0,10,1),
-    help='MNIST noisy number to generate'
+    default=np.random.randint(0,10),
+    help='MNIST noisy number to generate')
 
 def main(args):
-    X = torch.load(args.filename_X)
-    y = torch.load(args.filename_y)
+    X = torch.load(args.filename_X_clean)
+    y = torch.load(args.filename_y_clean)
+    yn = torch.load(args.filename_y_noisy)
     
-    print(X)
-    print(y)
+    indices = (y!=yn).nonzero(as_tuple=True)[0]
     
+    X = X[indices]
+    y = y[indices]
+    yn = yn[indices]
     
+    indices = (y==args.number).nonzero(as_tuple=True)[0]
     
+    X = X[indices]
+    y = y[indices]
+    yn = yn[indices]
     
-if __name__ == "__main__":
+    n = X.shape[0]
+    
+    num = np.random.randint(0,n)
+    
+    title = f"True: {y[num]}, Noise: {yn[num]}"
+    image = cv2.resize(X[num].squeeze(0).numpy(), (200, 200))
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
     main(parser.parse_args())
