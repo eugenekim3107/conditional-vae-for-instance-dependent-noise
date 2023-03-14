c96b7815162bc1e8fe5a2c550393eed5194d0493
eugenekim <eugenekim3107@gmail.com>
2023-03-14 04:02:26 -0700

    edited files

--- dataset.py
+++ dataset.py
@@ -17,3 +17,18 @@ class MNISTDataset(Dataset):
         if self.transform:
             X = self.transform(X)
         return X.to(torch.float32), y
+
+class MNISTNoiseDataset(Dataset):
+    def __init__(self, X_file, y_file, y_noise_file):
+        self.X = torch.load(X_file)
+        self.y = torch.load(y_file)
+        self.yn = torch.load(y_noise_file)
+    
+    def __len__(self):
+        return self.X.shape[0]
+    
+    def __getitem__(self, index):
+        X = self.X[index]
+        y = self.y[index]
+        yn = self.yn[index]
+        return X.to(torch.float32), y.to(torch.int64), yn.to(torch.int64)
